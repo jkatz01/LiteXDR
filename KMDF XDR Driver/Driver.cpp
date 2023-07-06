087c4f462a503c59e372514df48c58daa50012df
Yehonatan Katz <yehonatankatz@gmail.com>
2023-07-05 23:45:43 -0400

    test

--- KMDF XDR Driver/Driver.cpp
+++ KMDF XDR Driver/Driver.cpp
@@ -1,9 +1,11 @@
 #include <ntddk.h>
 #include <wdf.h>
+#include "Driver.h"
 
 DRIVER_UNLOAD KmdfDriverUnload;
 EVT_WDF_DRIVER_DEVICE_ADD KmdfHelloWorldEvtDeviceAdd;
 PRKMUTEX PrMutex; // TODO: put this in device context
+SINGLE_LIST_ENTRY ListHead;
 
 #define MAX_PR_NAME_SIZE 128
 
@@ -11,22 +13,18 @@ typedef struct {
     SINGLE_LIST_ENTRY SingleListEntry;
     CHAR ProcessName[MAX_PR_NAME_SIZE];
     SIZE_T ImageSize;
-}PR_INFO_ENTRY, *P_PR_INFO_ENTRY; //This creates a point that you can refer to as PXXX instead of XXX * i think???
+}PROC_INFO_ENTRY, *P_PROC_INFO_ENTRY; //This creates a point that you can refer to as PXXX instead of XXX * i think???
 
-void PushProcessEntry(PSINGLE_LIST_ENTRY ListHead, P_PR_INFO_ENTRY Entry) {
+void PushProcessEntry(PSINGLE_LIST_ENTRY _ListHead, P_PROC_INFO_ENTRY Entry) {
     KeWaitForSingleObject(PrMutex, Executive, KernelMode, FALSE, 0);
-    PushEntryList(ListHead, &(Entry->SingleListEntry));
+    PushEntryList(_ListHead, &(Entry->SingleListEntry));
     KeReleaseMutex(PrMutex, FALSE);
 }
 
-P_PR_INFO_ENTRY PopProcessEntry(PSINGLE_LIST_ENTRY ListHead) {
+P_PROC_INFO_ENTRY PopProcessEntry(PSINGLE_LIST_ENTRY _ListHead) {
     PSINGLE_LIST_ENTRY SingleListEntry;
-
-    KeWaitForSingleObject(PrMutex, Executive, KernelMode, FALSE, 0);
-    SingleListEntry = PopEntryList(ListHead);
-    KeReleaseMutex(PrMutex, FALSE);
-
-    return CONTAINING_RECORD(SingleListEntry, PR_INFO_ENTRY, SingleListEntry);
+    SingleListEntry = PopEntryList(_ListHead);
+    return CONTAINING_RECORD(SingleListEntry, PROC_INFO_ENTRY, SingleListEntry);
 }
 
 // Callback function for module load notification
@@ -37,7 +35,6 @@ VOID ModuleLoadCallback(
 )
 {
     UNREFERENCED_PARAMETER(ProcessId);
-    UNREFERENCED_PARAMETER(ImageInfo);
 
     if (FullImageName != NULL)
     {
@@ -46,7 +43,27 @@ VOID ModuleLoadCallback(
         KdPrintEx((DPFLTR_IHVDRIVER_ID, DPFLTR_ERROR_LEVEL, "Process name is %S  Size: %zu\n", FullImageName->Buffer, ImageInfo->ImageSize));
 
         //Create node to add in linked list
-        ExAllocatePool2(POOL_FLAG_PAGED, sizeof(PR_INFO_ENTRY), 1);
+        P_PROC_INFO_ENTRY ListNode = (P_PROC_INFO_ENTRY)ExAllocatePool2(POOL_FLAG_PAGED, sizeof(PROC_INFO_ENTRY), 1);
+        //RtlCopyMemory(ListNode->ProcessName, FullImageName->Buffer, (size_t)FullImageName->Length);
+        ListNode->ProcessName[0] = 'T';
+        ListNode->ProcessName[1] = 'E';
+        ListNode->ProcessName[2] = 'S';
+        ListNode->ProcessName[3] = 'T';
+        ListNode->ProcessName[4] = '\0';
+        ListNode->ImageSize = ImageInfo->ImageSize;
+
+        PushProcessEntry(&ListHead, ListNode);
+    }
+}
+
+VOID PopAllTemp() {
+    P_PROC_INFO_ENTRY entry;
+    while (true) {
+        if (ListHead.Next == NULL) {
+            return;
+        }
+        entry = PopProcessEntry(&ListHead);
+        ExFreePool2(entry, 1, NULL, 0); 
     }
 }
 
@@ -67,6 +84,9 @@ extern "C" {
 
         // Initialize mutex
         KeInitializeMutex(PrMutex, PASSIVE_LEVEL);
+
+        // Initialize linked list
+        ListHead.Next = NULL;
 
         // Print "Hello World" for DriverEntry
         DbgPrintEx(DPFLTR_IHVDRIVER_ID, DPFLTR_ERROR_LEVEL, "KmdfHelloWorld: DriverEntry    7777  \n");
@@ -138,6 +158,12 @@ VOID KmdfDriverUnload(_In_ PDRIVER_OBJECT DriverObject)
 
     // Unregister module load notification callback
     PsRemoveLoadImageNotifyRoutine(ModuleLoadCallback);
+    // now we know the callback will not be used
+    // mutex lock to make sure all existing callbacks ended
+    // mutex release 
+    KeWaitForSingleObject(PrMutex, Executive, KernelMode, FALSE, 0);
+    KeReleaseMutex(PrMutex, FALSE);
+    PopAllTemp();
 
     //DbgPrintEx(DPFLTR_IHVDRIVER_ID, DPFLTR_ERROR_LEVEL, "KmdfHelloWorld: BYE BYE  \n");
     KdPrintEx((DPFLTR_IHVDRIVER_ID, DPFLTR_ERROR_LEVEL, "KmdfHelloWorld: BYE BYE\n"));
