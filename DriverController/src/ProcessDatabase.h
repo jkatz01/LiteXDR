d214a3feab245d5c37c72942b382cd3b596af7f2
Yehonatan Katz <yehonatankatz@gmail.com>
2024-11-26 19:18:35 -0500

    Functions for sending hashes to server

--- DriverController/src/ProcessDatabase.h
+++ DriverController/src/ProcessDatabase.h
@@ -1,26 +1,31 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <string>
 #include <unordered_map>
+
+#include <curlpp/cURLpp.hpp>
+#include <curlpp/Easy.hpp>
+#include <curlpp/Options.hpp>
 
 #include "communication.h"
 
 #define TRUSTED_COUNT 5
 
 typedef struct {
-    int ImageSize;
+    size_t ImageSize;
     std::string ProcessName; 
 } ProcessData;
 
 typedef struct {
     ProcessData proc;
-    int Count;
+    size_t Count;
 } ProcessDataCounted;
 
 class ProcessDatabase
 {
 private:
-    std::unordered_map<size_t, ProcessDataCounted> ProcessHashmap;
+    std::unordered_map<size_t, ProcessDataCounted> LocalProcMap;
 public:
     std::string TestString = "test string";
     
@@ -34,9 +39,9 @@ public:
     
     void InsertProcess(ProcessData proc) {
         size_t hash = HashProcess(proc);
-        auto p = ProcessHashmap.find(hash);
-        if (p == ProcessHashmap.end()) {
-            ProcessHashmap.insert({ hash, {proc, 1} });
+        auto p = LocalProcMap.find(hash);
+        if (p == LocalProcMap.end()) {
+            LocalProcMap.insert({ hash, {proc, 1} });
         }
         else {
             p->second.Count++;
@@ -45,12 +50,12 @@ public:
 
     void InsertProcessCounted(ProcessDataCounted proc) {
         size_t hash = HashProcess(proc.proc);
-        ProcessHashmap.insert({ hash, proc });
+        LocalProcMap.insert({ hash, proc });
     }
 
     bool IsProcessTrusted(size_t hash) {
-        auto p = ProcessHashmap.find(hash);
-        if (p == ProcessHashmap.end()) {
+        auto p = LocalProcMap.find(hash);
+        if (p == LocalProcMap.end()) {
             return false;
         }
         else if (p->second.Count < TRUSTED_COUNT) {
@@ -62,18 +67,32 @@ public:
     }
     
     bool ProcessExists(size_t hash) {
-        if (ProcessHashmap.find(hash) == ProcessHashmap.end()) {
+        if (LocalProcMap.find(hash) == LocalProcMap.end()) {
             return false;
         }
         return true;
     }
 
+    size_t CountItemsInProcBuffer(PVOID Buffer, ULONG ReturnLength)
+    {
+        size_t i = 0;
+        size_t count = 0;
+        while (i < ReturnLength) {
+            size_t h_image_size = ((PPROCESS_HEADER)((char*)Buffer + i))->image_size;
+            size_t h_string_length = ((PPROCESS_HEADER)((char*)Buffer + i))->string_length;
+            i += sizeof(PROCESS_HEADER);
+            i += h_string_length * sizeof(WCHAR);
+            count++;
+        }
+        return count;
+    }
+
     void AddProcBufferToDatabase(PVOID Buffer, ULONG ReturnLength)
     {
-        SIZE_T i = 0;
+        size_t i = 0;
         while (i < ReturnLength) {
-            SIZE_T h_image_size = ((PPROCESS_HEADER)((char*)Buffer + i))->image_size;
-            SIZE_T h_string_length = ((PPROCESS_HEADER)((char*)Buffer + i))->string_length;
+            size_t h_image_size = ((PPROCESS_HEADER)((char*)Buffer + i))->image_size;
+            size_t h_string_length = ((PPROCESS_HEADER)((char*)Buffer + i))->string_length;
 
             i += sizeof(PROCESS_HEADER);
             
@@ -88,35 +107,95 @@ public:
             InsertProcess(p);
         }
     }
-    
-    void PrintProcessData(size_t hash) {
-        ProcessData *proc = &ProcessHashmap.at(hash).proc;
-        printf("HASH: %20zu SIZE: %5d NAME: %s\n", hash, proc->ImageSize, proc->ProcessName.c_str());
-    }
-    
-    void PrintTable() {
-        printf("TABLE SIZE: %zu\n", ProcessHashmap.size());
-        for (auto& i: ProcessHashmap)
-        {
-            printf("HASH: %20zu SIZE: %5d NAME: %s\n", (size_t)i.first, i.second.proc.ImageSize, i.second.proc.ProcessName.c_str());
+
+    void HashProcBuffer(PVOID Buffer, ULONG ReturnLength, std::vector<size_t> *hashes)
+    {
+        size_t i = 0;
+        while (i < ReturnLength) {
+            size_t h_image_size = ((PPROCESS_HEADER)((char*)Buffer + i))->image_size;
+            size_t h_string_length = ((PPROCESS_HEADER)((char*)Buffer + i))->string_length;
+
+            i += sizeof(PROCESS_HEADER);
+
+            PWCH string_buffer = ((PWCH)((char*)Buffer + i));
+            std::string proc_name;
+            proc_name.resize(h_string_length);
+            sprintf_s(&proc_name[0], h_string_length, "%S", string_buffer);
+
+            i += h_string_length * sizeof(WCHAR);
+
+            ProcessData p = { h_image_size, proc_name };
+            hashes->push_back(HashProcess(p));
         }
     }
 
-    SIZE_T GetTableSize() {
-        return ProcessHashmap.size();
+    void SendHashesToServer(PVOID Buffer, ULONG ReturnLength) {
+        std::vector<size_t> hashes;
+        HashProcBuffer(Buffer, ReturnLength, &hashes);
+
+        std::stringstream hashes_str(std::ios::binary);
+        for (size_t& h : hashes) {
+            hashes_str << h;
+        }
+
+        // send request
+        char url[22] = "http://127.0.0.1:8080";
+        try {
+            curlpp::Easy request;
+
+            request.setOpt(new curlpp::options::Url(url));
+            request.setOpt(new curlpp::options::Verbose(true));
+
+            std::list<std::string> header;
+            header.push_back("Content-Type: hash-send");
+
+            request.setOpt(new curlpp::options::HttpHeader(header));
+
+            // Send stringstream created from the vector<size_t> of hashes
+            // When reading in server, needs to load the stringstream into size_t
+            request.setOpt(new curlpp::options::PostFields(hashes_str.str()));
+            request.setOpt(new curlpp::options::PostFieldSize((long)hashes_str.str().size()));
+
+            request.perform();
+        }
+        catch (curlpp::LogicError& e) {
+            std::cout << e.what() << std::endl;
+        }
+        catch (curlpp::RuntimeError& e) {
+            std::cout << e.what() << std::endl;
+        }
+
+        // vector should free itself
+    }
+    
+    void PrintProcessData(size_t hash) {
+        ProcessData *proc = &LocalProcMap.at(hash).proc;
+        printf("HASH: %20zu SIZE: %5zu NAME: %s\n", hash, proc->ImageSize, proc->ProcessName.c_str());
+    }
+    
+    void PrintTable() {
+        printf("TABLE SIZE: %zu\n", LocalProcMap.size());
+        for (auto& i: LocalProcMap)
+        {
+            printf("HASH: %20zu SIZE: %5zu NAME: %s\n", (size_t)i.first, i.second.proc.ImageSize, i.second.proc.ProcessName.c_str());
+        }
+    }
+
+    size_t GetTableSize() {
+        return LocalProcMap.size();
     }
 
     void DeleteProcess(size_t hash) {
-        ProcessHashmap.erase(hash);
+        LocalProcMap.erase(hash);
     }
     
     void SaveDatabaseToFile() {
         std::ofstream savefile("database_save.bin", std::ios::binary);
 
-        int item_count = ProcessHashmap.size();
+        size_t item_count = LocalProcMap.size();
         savefile.write((char*)&item_count, sizeof(item_count));
 
-        for (auto& i : ProcessHashmap) {
+        for (auto& i : LocalProcMap) {
             savefile.write((char*)&i.second.proc.ImageSize, sizeof(i.second.proc.ImageSize));
 
             size_t size = i.second.proc.ProcessName.size();
@@ -132,12 +211,12 @@ public:
             std::cout << "Save file database_save.bin does not exist or failed" << std::endl;
             return;
         }
-        int item_count;
+        size_t item_count;
         savefile.read((char*)&item_count, sizeof(item_count));  // get item count
 
         for (int i = 0; i < item_count; i++) {
-            int x;
-            int c;
+            size_t x;
+            size_t c;
             savefile.read((char*)&x, sizeof(x));                // get ImageSize
             
             std::string str;
